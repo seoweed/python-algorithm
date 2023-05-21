ab9f44e49e583e975ea59d823245338b4e1e15f6
seoweed <darkweed1106@gmail.com>
2023-05-21 10:34:34 +0900

    버블정렬 알고리즘

---
+++ bubble_sort.py
@@ -0,0 +1,21 @@
+# 버블 정렬 알고리즘 
+list = [8,87,1,5,25,4,9,10]
+    cnt = 0
+    while 1:
+        for i in range(len(list)-1):
+            if list[i] > list[i + 1]:
+                a = list[i]
+                list[i] = list[i + 1]
+                list[i + 1] = a
+                cnt += 1
+        if cnt > 0:
+            cnt = 0
+        else:
+            return list
+            break
+    
+
+    
+
+b = bubble_sort(list)
+print(b)
